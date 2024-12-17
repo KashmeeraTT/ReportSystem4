cda82a511d85b4a80fee39811bfe10f5bddecc78
Tishan <tishan.21@cse.mrt.ac.lk>
2024-12-17 16:15:42 +0530

    up

--- frontend/src/components/ReportViewer/ReportViewer.jsx
+++ frontend/src/components/ReportViewer/ReportViewer.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./ReportViewer.css";
-import generateAERSection from "../AERAdvisory/generateAERSection";
+import generateAERSection from "../AERAdvisory/AERAdvisory/generateAERSection";
 
 const ReportViewer = ({ htmlReport, setUpdatedReportPages, district }) => {
     const [updatedReportPages, setUpdatedReportPagesState] = useState([]); // Updated pages
