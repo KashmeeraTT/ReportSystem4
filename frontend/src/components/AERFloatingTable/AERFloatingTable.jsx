32cc4dfdb0efcf6bb938a614206ddc470c2eef26
KashmeeraTT <tishan.21@cse.mrt.ac.lk>
2025-04-03 13:14:01 +0530

    New color

--- frontend/src/components/AERFloatingTable/AERFloatingTable.jsx
+++ frontend/src/components/AERFloatingTable/AERFloatingTable.jsx
@@ -88,7 +88,7 @@ const AERFloatingTable = ({ onSave, district }) => {
             overflow: hidden;
           }
           th {
-            background: linear-gradient(to right, #2980b9, #6dd5fa);
+            background: linear-gradient(to right,rgb(89, 188, 254),rgb(208, 226, 233));
             color: white;
             padding: 12px;
             text-align: center;
