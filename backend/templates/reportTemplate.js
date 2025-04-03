f06edb50e5985be814451792e029656a8d7c701f
KashmeeraTT <tishan.21@cse.mrt.ac.lk>
2025-04-03 18:26:11 +0530

    new

--- backend/templates/reportTemplate.js
+++ backend/templates/reportTemplate.js
@@ -4,35 +4,36 @@ module.exports = function generateReportTemplate(sections, district, day, month,
             <head>
                 <title>${district} District Agro-met Advisory Co-production Report - ${day} ${month} ${year}</title>
                 <style>
-                    body {
-                        font-family: Calibri, Arial, sans-serif;
-                        margin: 20px;
-                        line-height: 1.6;
-                        color: #333;
-                    }
-                    h1, h2, h3, h4 {
-                        font-family: Georgia, 'Times New Roman', serif; 
-                        text-align: left;
-                        color:rgb(0, 0, 0);
-                    }
-                    .section {
-                        margin-top: 20px;
-                    }
-                    table {
-                        width: 100%;
-                        border-collapse: collapse;
-                        margin: 20px 0;
-                    }
-                    table th, table td {
-                        border: 1px solid #ddd;
-                        padding: 8px;
-                        text-align: left;
-                    }
-                    table th {
-                        background-color: #f4f4f4;
-                        font-weight: bold;
-                        color: #555;
-                    }
+                body {
+                    font-family: "Segoe UI", Tahoma, sans-serif;
+                    margin: 20px;
+                    line-height: 1.6;
+                    color: #333;
+                }
+                h1, h2, h3, h4 {
+                    font-family: "Segoe UI", Tahoma, sans-serif;
+                    text-align: left;
+                    color: rgb(0, 0, 0);
+                }
+                p {
+                    font-family: "Segoe UI", Tahoma, sans-serif;
+                }
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                    margin: 20px 0;
+                    font-family: "Segoe UI", Tahoma, sans-serif;
+                }
+                table th, table td {
+                    border: 1px solid #ddd;
+                    padding: 8px;
+                    text-align: left;
+                }
+                table th {
+                    background-color: #f4f4f4;
+                    font-weight: bold;
+                    color: #555;
+                }
                 </style>
             </head>
             <body>
