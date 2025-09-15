af6662ab4f4f5d406db46fa7cb0a5dd67501ad58
KashmeeraTT <tishan,21@cse.mrt.ac.lk>
2025-09-15 16:45:18 +0000

    prod: update backend/server.js and backend/.env

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const app = require("./app");
 const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '127.0.0.1';
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log("Connected to MongoDB");
-        app.listen(process.env.PORT || 3000, () => {
+        app.listen(PORT, HOST, ()  => {
             console.log(`Server running on port ${process.env.PORT || 3000}`);
         });
     })
