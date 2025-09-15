352f325f8c9ff6cba9ad240dd90f802bf3e729a3
KashmeeraTT <tishan.21@cse.mrt.ac.lk>
2025-09-16 00:54:13 +0530

    db assets

--- backend/populate.js
+++ backend/populate.js
@@ -402,16 +402,16 @@ const meteorologyData = [
             png1: readImage(path.join(__dirname, 'dbAssets/AgroMet Data for Vavuniya', '2024-12-00-DAD-Water-Availability-Forecast-Minor-Tank.png')),
         },
     },
-    {
-        department: "DOA",
-        category: "Parameters",
-        month: "December",
-        year: 2024,
-        district: "Vavuniya",
-        content: {
-            text: readTextFile(path.join(__dirname, 'dbAssets/AgroMet Data for Vavuniya', '2024-12-00-DoA-Agromet-Parameter-Selection.txt')),
-        },
-    },
+    // {
+    //     department: "DOA",
+    //     category: "Parameters",
+    //     month: "December",
+    //     year: 2024,
+    //     district: "Vavuniya",
+    //     content: {
+    //         text: readTextFile(path.join(__dirname, 'dbAssets/AgroMet Data for Vavuniya', '2024-12-00-DoA-Agromet-Parameter-Selection.txt')),
+    //     },
+    // },
 
     {
         department: "DoM",
@@ -577,6 +577,409 @@ const meteorologyData = [
     //     content: {
     //         csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/AgroMet Data for April 2025", "2025-04-01-DAD-Reservoir-Water-Availability-Data-Small.csv")),
     //         png1: readImage(path.join(__dirname, 'dbAssets/AgroMet Data for April 2025', '2025-04-01-DAD-Water-Availability-Forecast-Minor-Tank.png')),
+    //     },
+    // },
+
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Seasonal",
+        month: "August",
+        year: 2025,
+        content: {
+            csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Rainfall-Forecast-Seasonal-and-Monthly-Data.csv")), // Store CSV as binary
+            png2: readImage(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Rainfall-Forecast-Seasonal.png")), // Store image as Buffer
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Monthly",
+        month: "August",
+        submonth: "August",
+        year: 2025,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Rainfall-Forecast-Monthly1.png")), // Store image as Buffer
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Monthly",
+        month: "August",
+        submonth: "September",
+        year: 2025,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Rainfall-Forecast-Monthly2.png")), // Store image as Buffer
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Monthly",
+        month: "August",
+        submonth: "October",
+        year: 2025,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Rainfall-Forecast-Monthly3.png")), // Store image as Buffer
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Weekly",
+        district: "Puttalam",
+        year: 2025,
+        weekNumber: 35,
+        subweekNumber: 0,
+        content: {
+            csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Rainfall-Forecast-Weekly-Data.csv")), // Store CSV as binary
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Weekly",
+        district: "Puttalam",
+        year: 2025,
+        weekNumber: 35,
+        subweekNumber: 1,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Rainfall-Forecast-Weekly1.png")),
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Weekly",
+        district: "Puttalam",
+        year: 2025,
+        weekNumber: 35,
+        subweekNumber: 2,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Rainfall-Forecast-Weekly2.png")),
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Weekly",
+        district: "Puttalam",
+        year: 2025,
+        weekNumber: 35,
+        subweekNumber: 3,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Rainfall-Forecast-Weekly3.png")),
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Weekly",
+        district: "Puttalam",
+        year: 2025,
+        weekNumber: 35,
+        subweekNumber: 4,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Rainfall-Forecast-Weekly4.png")),
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Recieved",
+        month: "July",
+        year: 2025,
+        district: "Puttalam",
+        content: {
+            csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/PUT", "2025-08-00-DoM-Observed-Rainfall-Anomaly-Data.csv")), // Store CSV as binary
+            png1: readImage(path.join(__dirname, 'dbAssets/PUT', '2025-08-00-DoM-Received-Monthly-Rainfall.png')),
+            png2: readImage(path.join(__dirname, 'dbAssets/PUT', '2025-08-00-DoM-Observed-Percent-of-Normal-Precipitation.png')),
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Climatological",
+        month: "August",
+        year: 2025,
+        district: "Puttalam",
+        content: {
+            text: readTextFile(path.join(__dirname, 'dbAssets/PUT', '2025-08-00-DoA-Agromet-Advisory-Summary.txt')),
+        },
+    },
+    {
+        department: "ID",
+        category: "Reservoir",
+        subcategory: "Major",
+        day: 31,
+        month: "August",
+        year: 2025,
+        district: "Puttalam",
+        content: {
+            csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/PUT", "2025-08-31-ID-Reservoir-Water-Availability-Data-Major.csv")),
+        },
+    },
+    {
+        department: "ID",
+        category: "Reservoir",
+        subcategory: "Medium",
+        day: 31,
+        month: "August",
+        year: 2025,
+        district: "Puttalam",
+        content: {
+            csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/PUT", "2025-08-31-ID-Reservoir-Water-Availability-Data-Medium.csv")),
+            //csv2: readCsvAsBinary(path.join(__dirname, "dbAssets/PUT", "2025-08-31-PID-Reservoir-Water-Availability-Data-Medium.csv")),
+        },
+    },
+    {
+        department: "DAD",
+        category: "Reservoir",
+        subcategory: "Minor",
+        day: 31,
+        month: "August",
+        year: 2025,
+        district: "Puttalam",
+        content: {
+            //csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/PUT", "2025-08-31-DAD-Reservoir-Water-Availability-Data-Small.csv")),
+            png1: readImage(path.join(__dirname, 'dbAssets/PUT', '2025-08-00-DAD-Water-Availability-Forecast-Minor-Tank.png')),
+        },
+    },
+    // {
+    //     department: "DOA",
+    //     category: "Parameters",
+    //     month: "August",
+    //     year: 2025,
+    //     district: "Puttalam",
+    //     content: {
+    //         text: readTextFile(path.join(__dirname, 'dbAssets/PUT', '2025-08-00-DoA-Agromet-Parameter-Selection.txt')),
+    //     },
+    // },
+    // // Add other data here in similar fashion, using `readImage` for png1, png2, and png3
+    // {
+    //     department: "ID",
+    //     category: "Reservoir",
+    //     subcategory: "Major",
+    //     day: 5,
+    //     month: "December",
+    //     year: 2024,
+    //     district: "Vavuniya",
+    //     content: {
+    //         csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/AgroMet Data for Vavuniya", "2024-12-05-ID-Reservoir-Water-Availability-Data-Major.csv")),
+    //     },
+    // },
+    // {
+    //     department: "ID",
+    //     category: "Reservoir",
+    //     subcategory: "Medium",
+    //     day: 5,
+    //     month: "December",
+    //     year: 2024,
+    //     district: "Vavuniya",
+    //     content: {
+    //         csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/AgroMet Data for Vavuniya", "2024-12-05-ID-Reservoir-Water-Availability-Data-Medium.csv")),
+    //         csv2: readCsvAsBinary(path.join(__dirname, "dbAssets/AgroMet Data for Vavuniya", "2024-12-05-PID-Reservoir-Water-Availability-Data-Medium.csv")),
+    //     },
+    // },
+    // {
+    //     department: "DAD",
+    //     category: "Reservoir",
+    //     subcategory: "Minor",
+    //     day: 5,
+    //     month: "December",
+    //     year: 2024,
+    //     district: "Vavuniya",
+    //     content: {
+    //         csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/AgroMet Data for Vavuniya", "2024-12-05-DAD-Reservoir-Water-Availability-Data-ASC.csv")),
+    //         png1: readImage(path.join(__dirname, 'dbAssets/AgroMet Data for Vavuniya', '2024-12-00-DAD-Water-Availability-Forecast-Minor-Tank.png')),
+    //     },
+    // },
+    // {
+    //     department: "DOA",
+    //     category: "Parameters",
+    //     month: "December",
+    //     year: 2024,
+    //     district: "Vavuniya",
+    //     content: {
+    //         text: readTextFile(path.join(__dirname, 'dbAssets/AgroMet Data for Vavuniya', '2024-12-00-DoA-Agromet-Parameter-Selection.txt')),
+    //     },
+    // },
+
+        {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Seasonal",
+        month: "August",
+        year: 2025,
+        content: {
+            csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Rainfall-Forecast-Seasonal-and-Monthly-Data.csv")), // Store CSV as binary
+            png2: readImage(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Rainfall-Forecast-Seasonal.png")), // Store image as Buffer
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Monthly",
+        month: "August",
+        submonth: "August",
+        year: 2025,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Rainfall-Forecast-Monthly1.png")), // Store image as Buffer
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Monthly",
+        month: "August",
+        submonth: "September",
+        year: 2025,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Rainfall-Forecast-Monthly2.png")), // Store image as Buffer
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Monthly",
+        month: "August",
+        submonth: "October",
+        year: 2025,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Rainfall-Forecast-Monthly3.png")), // Store image as Buffer
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Weekly",
+        district: "Kurunegala",
+        year: 2025,
+        weekNumber: 35,
+        subweekNumber: 0,
+        content: {
+            csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Rainfall-Forecast-Weekly-Data.csv")), // Store CSV as binary
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Weekly",
+        district: "Kurunegala",
+        year: 2025,
+        weekNumber: 35,
+        subweekNumber: 1,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Rainfall-Forecast-Weekly1.png")),
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Weekly",
+        district: "Kurunegala",
+        year: 2025,
+        weekNumber: 35,
+        subweekNumber: 2,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Rainfall-Forecast-Weekly2.png")),
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Weekly",
+        district: "Kurunegala",
+        year: 2025,
+        weekNumber: 35,
+        subweekNumber: 3,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Rainfall-Forecast-Weekly3.png")),
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Weekly",
+        district: "Kurunegala",
+        year: 2025,
+        weekNumber: 35,
+        subweekNumber: 4,
+        content: {
+            png1: readImage(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Rainfall-Forecast-Weekly4.png")),
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Recieved",
+        month: "July",
+        year: 2025,
+        district: "Kurunegala",
+        content: {
+            csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/KUR", "2025-08-00-DoM-Observed-Rainfall-Anomaly-Data.csv")), // Store CSV as binary
+            png1: readImage(path.join(__dirname, 'dbAssets/KUR', '2025-08-00-DoM-Received-Monthly-Rainfall.png')),
+            png2: readImage(path.join(__dirname, 'dbAssets/KUR', '2025-08-00-DoM-Observed-Percent-of-Normal-Precipitation.png')),
+        },
+    },
+    {
+        department: "DoM",
+        category: "Rainfall",
+        subcategory: "Climatological",
+        month: "August",
+        year: 2025,
+        district: "Kurunegala",
+        content: {
+            text: readTextFile(path.join(__dirname, 'dbAssets/KUR', '2025-08-00-DoA-Agromet-Advisory-Summary.txt')),
+        },
+    },
+    {
+        department: "ID",
+        category: "Reservoir",
+        subcategory: "Major",
+        day: 31,
+        month: "August",
+        year: 2025,
+        district: "Kurunegala",
+        content: {
+            csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/KUR", "2025-08-31-ID-Reservoir-Water-Availability-Data-Major.csv")),
+        },
+    },
+    {
+        department: "ID",
+        category: "Reservoir",
+        subcategory: "Medium",
+        day: 31,
+        month: "August",
+        year: 2025,
+        district: "Kurunegala",
+        content: {
+            csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/KUR", "2025-08-31-ID-Reservoir-Water-Availability-Data-Medium.csv")),
+            //csv2: readCsvAsBinary(path.join(__dirname, "dbAssets/KUR", "2025-08-31-PID-Reservoir-Water-Availability-Data-Medium.csv")),
+        },
+    },
+    {
+        department: "DAD",
+        category: "Reservoir",
+        subcategory: "Minor",
+        day: 31,
+        month: "August",
+        year: 2025,
+        district: "Kurunegala",
+        content: {
+            //csv1: readCsvAsBinary(path.join(__dirname, "dbAssets/KUR", "2025-08-31-DAD-Reservoir-Water-Availability-Data-Small.csv")),
+            png1: readImage(path.join(__dirname, 'dbAssets/KUR', '2025-08-00-DAD-Water-Availability-Forecast-Minor-Tank.png')),
+        },
+    },
+    // {
+    //     department: "DOA",
+    //     category: "Parameters",
+    //     month: "August",
+    //     year: 2025,
+    //     district: "Kurunegala",
+    //     content: {
+    //         text: readTextFile(path.join(__dirname, 'dbAssets/KUR', '2025-08-00-DoA-Agromet-Parameter-Selection.txt')),
     //     },
     // },
 ];
